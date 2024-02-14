3f158ad268ebad882b5dc32359538779abb97fb6
jerson <ing.jersonvilla@gmail.com>
2024-02-14 11:15:41 -0500

    Ajuste test unitarios controller
    Ajuste test unitarios controller

--- src/test/java/com/springboot/alianza/apirest/controllers/ClienteRestControllerTest.java
+++ src/test/java/com/springboot/alianza/apirest/controllers/ClienteRestControllerTest.java
@@ -14,124 +14,70 @@ import com.springboot.alianza.apirest.dto.response.ClienteResponseDto;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.alianza.apirest.models.entity.Cliente;
 import com.springboot.alianza.apirest.services.IClienteService;
 
 @SpringBootTest
-@AutoConfigureMockMvc
-@WebAppConfiguration
 public class ClienteRestControllerTest {
-
-	private final static String URL = "/api/clientes";
-
-	@Autowired
-	MockMvc mockMvc;
-
 	@Mock
 	IClienteService clienteService;
-
 	@InjectMocks
 	ClienteRestController clienteRestController;
-
 	@Test
 	void findAllCustomer() {
 		List<ClienteDto> clientes = new ArrayList<>();
 		clientes.add(new ClienteDto(1L, "jvillamizar", "Jerson Villamizar", "jvillamizar@gmail.com", "31546507624", new Date()));
 		clientes.add(new ClienteDto(2L, "ogarcia", "Oscar Garcia", "ogarcia@gmail.com", "3154650762", new Date()));
+		ClienteResponseDto clienteResponseDto = new ClienteResponseDto();
+		clienteResponseDto.setClientes(clientes);
 
-		when(clienteService.findAllCustomer()).thenReturn(new GeneralResponse<ClienteResponseDto>(
-				ClienteResponseDto.builder().clientes(clientes).build(), HttpStatus.OK.getReasonPhrase()));
+		when(clienteService.findAllCustomer())
+				.thenReturn(new GeneralResponse<>(HttpStatus.OK, "OK", ClienteResponseDto.builder().clientes(clientes).build()));
 
-		// Llamar al método del controlador que queremos probar
 		GeneralResponse<ClienteResponseDto> response = clienteRestController.findAllCustomer();
 
-		// Verificar que la respuesta no sea nula
 		assertNotNull(response);
-
-		// Verificar que el campo data no sea nulo
 		assertNotNull(response.getData());
 
-		// Verificar que la lista de clientes no sea nula y tenga el tamaño esperado
 		List<ClienteDto> clientesEnRespuesta = response.getData().getClientes();
 		assertNotNull(clientesEnRespuesta);
 		assertEquals(2, clientesEnRespuesta.size());
 	}
-
-
 	@Test
 	void findCustomerById() throws Exception {
 
+		Long Id = 1L;
 		List<ClienteDto> clientes = new ArrayList<>();
-		clientes.add(new ClienteDto(1L, "jvillamizar", "Jerson Villamizar", "jvillamizar@gmail.com", "31546507624", new Date()));
-		ClienteResponseDto clienteResponseDto = new ClienteResponseDto();
-		clienteResponseDto.setClientes(clientes);
 
 		when(clienteService.findCustomerById(1L))
 				.thenReturn(new GeneralResponse<>(HttpStatus.OK, "OK", ClienteResponseDto.builder().clientes(clientes).build()));
 
+		GeneralResponse<ClienteResponseDto> response = clienteRestController.findCustomerById(Id);
 
-		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(URL)
-						.accept(MediaType.APPLICATION_JSON_VALUE)
-						.contentType(MediaType.APPLICATION_JSON_VALUE)
-						.content(mapToJson(clientes)))
-				.andReturn();
-
-		assertEquals(200, result.getResponse().getStatus());
+		assertEquals(HttpStatus.OK, response.getHttpStatus());
 	}
-
 	@Test
-	void saveCustomer() throws Exception{
-		Cliente cliente = buildCliente();
+	void saveCustomerTest() throws Exception {
 
-		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(URL)
-						.accept(MediaType.APPLICATION_JSON_VALUE)
-						.contentType(MediaType.APPLICATION_JSON_VALUE)
-						.content(mapToJson(cliente)))
-				.andReturn();
+		when(clienteService.saveCustomer(new Cliente())).thenReturn(
+				new GeneralResponse<>(HttpStatus.OK, "OK", new ClienteDto()));
 
-		assertEquals(200, result.getResponse().getStatus());
+		GeneralResponse<ClienteDto> response = clienteRestController.saveCustomer(new Cliente());
+
+		assertEquals(HttpStatus.OK, response.getHttpStatus());
 	}
 
 	@Test
 	void updateCustomer() throws Exception{
-		Cliente cliente = buildCliente();
+		Long Id = 1L;
+		when(clienteService.updateCustomer(new Cliente(), Id)).thenReturn(
+				new GeneralResponse<>(HttpStatus.OK, "OK", new ClienteDto()));
 
-		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(URL.concat("/1"))
-						.accept(MediaType.APPLICATION_JSON_VALUE)
-						.contentType(MediaType.APPLICATION_JSON_VALUE)
-						.content(mapToJson(cliente)))
-				.andReturn();
+		GeneralResponse<ClienteDto> response = clienteRestController.updateCustomer(new Cliente(), Id);
 
-		assertEquals(200, result.getResponse().getStatus());
-	}
-
-	private Cliente buildCliente() {
-		Cliente cliente = new Cliente();
-		cliente.setId(1L);
-		cliente.setSharedKey("ogarcia");
-		cliente.setBusinessId("Oscar Garcia");
-		cliente.setEmail("ogarcia@gmail.com");
-		cliente.setPhone("3154650761");
-		cliente.setDataAdd(new Date());
-
-		return cliente;
-	}
-
-	private String mapToJson(Object object) throws JsonProcessingException{
-		ObjectMapper mapper = new ObjectMapper();
-		return mapper.writeValueAsString(object);
+		assertEquals(HttpStatus.OK, response.getHttpStatus());
 	}
 	
 }
